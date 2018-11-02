14a57dc0663ddcd4ace01275135b36cf7440a701
Rose Kunkel <rose@rosekunkel.me>
2018-11-02 15:00:50 -0700

    Add script to get the strongest classifier for a set of phones.

---
+++ scripts/strongest_classifier.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import csv, sys
+from functools import reduce
+
+def read_phones(filename):
+  with open(filename, "r") as f:
+    reader = csv.DictReader(f)
+    return list(reader)
+
+def intersect(phone_1, phone_2):
+  return dict(phone_1.items() & phone_2.items())
+
+def print_phone(phone):
+  for (feature, value) in phone.items():
+    if value == "+":
+      print(feature)
+    elif value == "-":
+      print("not " + feature)
+
+if __name__ == "__main__":
+  filename = sys.argv[1]
+  phones = read_phones(filename)
+  selected_phones = filter(lambda phone: phone["symbol"] in sys.argv[2:], phones)
+  classifier = reduce(intersect, selected_phones)
+  print_phone(classifier)

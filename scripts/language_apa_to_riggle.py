233fb79dd866f5ce51f48112af052cb1735c2536
Qian Xiong <qx2dk@virginia.edu>
2018-11-14 16:18:20 -0800

    script for generating riggles.csv for a language

---
+++ scripts/language_apa_to_riggle.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+"""Generate [language_name]_riggle.csv file (similar to riggle.csv) for a specific language,
+ by removing phonetic symbols not in the language from riggle.py/riggle.csv"""
+
+# usage example: python3 language_apa_to_riggle.py ../datasets/AmharicAPA.csv
+# [Language]APA.csv files can be found in the Dataset Files folder in the Team Drive
+
+import riggle, argparse, csv
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_csv_file', help='an csv file where the first column are words in APA symbols')
+    args = parser.parse_args()
+
+    symbols = set()
+    with open(args.input_csv_file)as f:
+        for line in csv.reader(f):
+            phone, meaning = line
+            symbols.update(phone)
+
+    # print(symbols)
+
+    # hard-coded order of features (same order as in riggle.csv)
+    features = ["voice", "front", "nas", "back", "round", "cons", "cont", "coronal", "ATR", "dist", "symbol",
+                "del. rel.", "c.g.", "approx", "labial", "high", "strid", "ant", "dorsal", "s.g.", "son", "lat", "syll",
+                "low"]
+
+    print(','.join(features))
+    for sym in riggle.symbols:
+        if sym['symbol'] in symbols:
+            print(','.join(sym[f] for f in features))
+
+
+if __name__ == '__main__':
+    main()

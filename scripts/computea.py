9b29dcbc288c36fd4c8d5b0965dc84959fa1c584
Shraddha Barke <shraddha.6596@gmail.com>
2018-12-02 19:25:16 -0800

    added birectional

--- scripts/computea.py
+++ scripts/computea.py
@@ -1,6 +1,7 @@
 import csv, sys, subprocess, json
-from strongest_classifier import context, classifier, read_phone, remove_indices, add_indices
-from implied import all_same, inferred
+from collections import ChainMap
+from strongest_classifier import weakest_classifier, strongest_sound_classifier, strongest_context_classifier, remove_pos, read_phone, remove_indices, add_indices
+from implied import all_same
 
 def convertdict(dictname):
 	clist = []
@@ -15,6 +16,17 @@ def convertlist(flist):
 		cdict.update({elem[1:]:elem[0]})
 	return cdict
 
+def intersectdict(dict_1, dict_2):
+	'''Returns intersection of dictionaries'''
+	return dict(dict_1.items() & dict_2.items())
+
+def diffdict(dict_1, dict_2):
+	'''Returns the symmetric difference between dictionaries'''
+	return dict(dict_1.items() ^ dict_2.items())
+
+def mergedict(dict_1, dict_2):
+	return dict(ChainMap(dict_1, dict_2))
+
 def mod_implied(data, feature_1, feature_2, output):
 	imply = []
 	f1 = data[feature_1]
@@ -25,7 +37,30 @@ def mod_implied(data, feature_1, feature_2, output):
 		imply.append((output[feature_1]+feature_1,output[feature_2]+feature_2))
 	return imply
 
-def check_noisy(data,dictname,context):
+def extract_feature_value(data, dictname, featuredict):
+	valuelist = list(dictname.values())
+	remove_pos(data,featuredict)
+	for feature, value in featuredict.items():
+		llist = []
+		valuelist.append(value)
+		dictname.update({feature : value})
+		for key, values in dictname.items():
+			llist.append(data[key])
+		nllist = [list(zip(*llist))[i] for i in range(len(llist[0]))]
+		sindices = [i for i, x in enumerate(nllist) if (x == tuple(valuelist))]
+		print(sindices)
+		if(len(sindices)) == 0:
+			for elem in sindices:
+				print(data["symbol"][elem])
+			pass
+		else:
+			for elem in sindices:
+				print(data["symbol"][elem])
+			valuelist.remove(value)
+			dictname.pop(feature,None)
+	return dictname
+
+def check_noisy(data, dictname, trueneg):
 	llist = []
 	contextlist = []
 	valuelist = list(dictname.values())
@@ -33,12 +68,13 @@ def check_noisy(data,dictname,context):
 	for feature, value in dictname.items():
 		llist.append(remove_indices(target_indices, data[feature]))
 	nllist = [list(zip(*llist))[i] for i in range(len(llist[0]))]
+	print(nllist)
 	sindices = [i for i, x in enumerate(nllist) if (x == tuple(valuelist))]
 	print("The following sounds might be noisy!")
 	for elem in sindices:
 		print(data["symbol"][elem])
-	contextvalues = list(context.values())
-	for feature,value in context.items():
+	contextvalues = list(weakcontext.values())
+	for feature,value in weakcontext.items():
 		contextlist.append(add_indices(sindices, data[feature]))
 	ncontextlist = [list(zip(*contextlist))[i] for i in range(len(contextlist[0]))]
 	cindices = [i for i, x in enumerate(ncontextlist) if (x == tuple(contextvalues))]
@@ -51,15 +87,30 @@ def check_noisy(data,dictname,context):
 if __name__ == "__main__":
 	fname = sys.argv[1]
 	riggle = sys.argv[2]
+	contextfname = sys.argv[3]
+	soundfname = sys.argv[4]
 	data = read_phone(fname)
 	riggledata = read_phone(riggle)
-	contextfname = sys.argv[3]
-	context = context(contextfname)
-	dictname = classifier(fname)
-	keylist = list(dictname.keys())
-	inferred = [mod_implied(riggledata, p1, p2, dictname) for p1 in keylist for p2 in keylist if (p1 != p2 and p1 != 'symbol' and p2 != 'symbol')]
+	weakcontext = weakest_classifier(contextfname)
+	weaksound = weakest_classifier(soundfname)
+	sounddict = strongest_sound_classifier(fname)
+	strongcontext = strongest_context_classifier(fname)
+	soundkeys = list(sounddict.keys())
+	inferred = [mod_implied(riggledata, p1, p2, sounddict) for p1 in soundkeys for p2 in soundkeys if (p1 != p2 and p1 != 'symbol' and p2 != 'symbol')]
 	elems = [x[0][1] for x in inferred if x != []]
-	computea = [x for x in convertdict(dictname) if x not in elems]
-	print(context)
-	check_noisy(data,convertlist(computea),context)
-	print(computea)
+	strongsound = convertlist([x for x in convertdict(sounddict) if x not in elems])
+	#print(weakcontext)
+	#print(strongsound)
+	#print(strongcontext)
+	#print(weaksound)
+	intersect_sound = intersectdict(weaksound,strongsound)
+	intersect_context = intersectdict(weakcontext,strongcontext)
+	diff_sound = diffdict(weaksound,strongsound)
+	diff_context = diffdict(weakcontext,strongcontext)
+	#print(intersect_sound,intersect_context)
+	#print(diff_sound, diff_context)
+	#check_noisy(data,strongsound,weakcontext)
+	mergeddict = mergedict(intersect_sound, intersect_context)
+	remove_pos(data, mergeddict)
+	print(extract_feature_value(data, mergeddict, diff_sound))
+	print(extract_feature_value(data, mergeddict, diff_context))
--- scripts/strongest_classifier.py
+++ scripts/strongest_classifier.py
@@ -4,6 +4,7 @@ import csv, sys
 from functools import reduce
 
 def read_phone(filename):
+  '''Returns a dictionary with key : feature name and value: list of feature values for all sounds.'''
   with open(filename, 'r') as infile:
     reader = csv.DictReader(infile)
     data = {}
@@ -16,6 +17,7 @@ def read_phone(filename):
   return data
 
 def read_phones(filename):
+  '''Returns a list of OrderedDict for each symbol and it's features.'''
   with open(filename, "r") as f:
     reader = csv.DictReader(f)
     return list(reader)
@@ -23,36 +25,52 @@ def read_phones(filename):
 def intersect(phone_1, phone_2):
   return dict(phone_1.items() & phone_2.items())
 
-def remove_indices(index_list,target_list):
-  for index in sorted(index_list, reverse=True):
-    target_list.pop(index)
-  return target_list
+def remove_indices(lindex, ltarget):
+  '''Removes the elements of ltarget at indices present in lindex.'''
+  for index in sorted(lindex, reverse=True):
+      ltarget.pop(index)
 
-def add_indices(index_list,target_list):
+def add_indices(lindex, ltarget):
+  '''Adds the elements of ltarget at indices present in lindex'''
   ntarget = []
-  for index in sorted(index_list, reverse=True):
-    ntarget.append(target_list[index])
+  for index in sorted(lindex, reverse=True):
+    ntarget.append(ltarget[index])
   return ntarget
 
-def check_neg(filename,dictionary):
-  data = read_phone(filename)
-  ndict = dictionary.copy()
+def remove_pos(data, dictname):
   target_indices = ([i for i, x in enumerate(data["target"]) if x == "1"])
-  for key,value in dictionary.items():
-    if any(value == x for x in remove_indices(target_indices, data[key])):
+  for key, value in dictname.items():
+    remove_indices(target_indices, data[key])
+
+def check_negative(filename, dictname):
+  '''Removes those features from dictname that are not consistent with negative examples'''
+  data = read_phone(filename)
+  posexamples = ([i for i, x in enumerate(data["target"]) if x == "1"])
+  remove_pos(data, dictname)
+  ndict = dictname.copy()
+  for key,value in dictname.items():
+    if any(value == x for x in data[key]):
       ndict.pop(key,None)
   return ndict
 
-def context(filename):
+def weakest_classifier(filename):
   phones = read_phones(filename)
   selected_phones = filter(lambda phone: phone["target"] == "1", phones)
   classifier = reduce(intersect, selected_phones)
-  new_classifier = check_neg(filename,classifier)
-  return new_classifier
+  nclassifier = check_negative(filename,classifier)
+  nclassifier.pop("target",None)
+  return nclassifier
 
-def classifier(filename):
+def strongest_sound_classifier(filename):
   phones = read_phones(filename)
   selected_phones = filter(lambda phone: phone["target"] == "1", phones)
   classifier = reduce(intersect, selected_phones)
-  new_classifier = {k:v for (k, v) in classifier.items() if not k.startswith('L') and not k.startswith('R') and k != "target"}
-  return new_classifier
+  nclassifier = {k : v for (k, v) in classifier.items() if not k.startswith('L') and not k.startswith('R') and k != "target"}
+  return nclassifier
+
+def strongest_context_classifier(filename):
+  phones = read_phones(filename)
+  selected_phones = filter(lambda phone: phone["target"] == "1", phones)
+  classifier = reduce(intersect, selected_phones)
+  nclassifier = {k : v for (k, v) in classifier.items() if k.startswith('L') or k.startswith('R') and k != "target"}
+  return nclassifier

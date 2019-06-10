19fa44551b26e131bebc38295d6e60473b6aff56
Shraddha Barke <shraddha.6596@gmail.com>
2019-06-10 04:46:03 -0700

    .

--- src/sat.py
+++ src/sat.py
@@ -140,7 +140,7 @@ def infer_condition(triples_changed):
   opt.add(likelihood == log_num_models * LIKELIHOOD_WEIGHT * n_pos)
 
   objective = z3.Int('objective')
-  opt.add(objective == simplicity + likelihood)
+  opt.add(objective == simplicity)
   opt.minimize(objective)
 
   alphas = {}
@@ -213,20 +213,19 @@ def infer_condition(triples_changed):
       print(f'Likelihood: {model[likelihood]}')
       return rule
       opt.add(z3.Not(z3.And(*new_model_constraint)))
-    #else:
-      #solver.check()
-      #unsat_core = solver.unsat_core()
-      #print('\033[1;31mUnsatisfiable constraints:\033[0;31m') # Set text color to red
-      #for name in unsat_core:
-      #  if str(name) in formulas:
-      #    formula, (l, c, r), changed = formulas[str(name)]
-      #    changed_str = 'changed' if changed else "didn't change"
-      #    print(f'/{l} . {c} . {r}/ {changed_str}')
-      #  else:
-      #    print(f'{str(name)}')
-      #print('\033[0;0m') # Reset text color and add a newline
+    else:
+      solver.check()
+      unsat_core = solver.unsat_core()
+      print('\033[1;31mUnsatisfiable constraints:\033[0;31m') # Set text color to red
+      for name in unsat_core:
+        if str(name) in formulas:
+          formula, (l, c, r), changed = formulas[str(name)]
+          changed_str = 'changed' if changed else "didn't change"
+          print(f'/{l} . {c} . {r}/ {changed_str}')
+        else:
+          print(f'{str(name)}')
+      print('\033[0;0m') # Reset text color and add a newline
       return None
-      #return None
   print('')
 
 LOG_NUM_MODELS = 0
